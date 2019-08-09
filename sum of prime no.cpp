93a06aaa026e3b8fc88547e02c00c6ddb75820aa
praveen260 <53816605+praveen260@users.noreply.github.com>
2019-08-09 16:07:07 +0530

    Add files via upload

---
+++ sum of prime no.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+   string raptor_prompt_variable_zzyz;
+   ?? j;
+   ?? b;
+   ?? c;
+   ?? i;
+   ?? sum;
+
+   raptor_prompt_variable_zzyz ="enter a value ";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> i;
+   raptor_prompt_variable_zzyz ="enter b value";
+   cout << raptor_prompt_variable_zzyz << endl;
+   cin >> b;
+   sum =0;
+   while (1)
+   {
+      c =0;
+      if (i>b)) break;
+      j =1;
+      while (!(j==i))
+      {
+         if (i % j==0)
+         {
+            c =c+1;
+         }
+         else
+         {
+         }
+         j =j+1;
+      }
+      if (c>=2)
+      {
+      }
+      else
+      {
+         sum =sum+i;
+      }
+      i =i+1;
+   }
+   cout << sum << endl;
+   return 0;
+}
